c40da01593ea8b3ba76467aa0e37e5f5b29ee246
Artem Vasylenko <avasylenko@revenuewell.com>
2021-09-12 12:18:31 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Android Material Component: Navigation Drawer Styling
+
+## Tutorial:
+Part I: https://www.waseefakhtar.com/android/easy-approach-to-navigation-drawer/ </br>
+Part II: https://www.waseefakhtar.com/android/android-material-component-navigation-drawer-styling-part-ii/ </br>
+Part III: https://www.waseefakhtar.com/android/android-material-component-navigation-drawer-styling-part-iii/ </br>
+
+### Screenshots
+
+<img src="https://github.com/waseefakhtar/Navigation-Drawer/blob/master/Screenshots/Part-1.png?raw=true" width="256"/> <img src="https://github.com/waseefakhtar/Navigation-Drawer/blob/master/Screenshots/Part-2.png?raw=true" width="256"/>
+<img src="https://github.com/waseefakhtar/Navigation-Drawer/blob/master/Screenshots/Part-3.png?raw=true" width="256"/> <img src="https://github.com/waseefakhtar/Navigation-Drawer/blob/master/Screenshots/Part-4.png?raw=true" width="256"/>
---
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        navigationView.setNavigationItemSelectedListener {
+            true
+        }
+
+        val menuItem = navigationView.menu.findItem(R.id.nav_share)
+        val switch = menuItem.actionView as SwitchCompat
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            // TODO: Switch animation doesn't work after adding this line!
+            navigationView.menu.findItem(R.id.nav_home).isEnabled = isChecked
+        }
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
